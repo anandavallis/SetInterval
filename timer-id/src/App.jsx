4dd5d3c76ad11e6ad23a8d01e2cc3ff7f537a7b1
anandavallis <sanandavalli7@gmail.com>
2025-07-05 14:31:04 +0530

    Create App.jsx

---
+++ timer-id/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Stopwatch from './components/Stopwatch';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Stopwatch App</h1>
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
